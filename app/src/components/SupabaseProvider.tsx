a6315efda5b732281da7edabb019f046e594a94e
Szilard DOBAI <dobai_szilard@yahoo.com>
2023-09-06 00:55:11 +0300

    Fix TS.

--- app/src/components/SupabaseProvider.tsx
+++ app/src/components/SupabaseProvider.tsx
@@ -2,7 +2,7 @@ import { Database } from "@audiocalendar/types";
 import { SupabaseClient, createClient } from "@supabase/supabase-js";
 import { createContext, useMemo, type PropsWithChildren } from "react";
 
-type SupabaseContextType = SupabaseClient | null;
+type SupabaseContextType = SupabaseClient<Database> | null;
 
 const createSupabaseClient = () => {
   const supabaseUrl = import.meta.env.VITE_SUPABASE_PROJECT_URL;

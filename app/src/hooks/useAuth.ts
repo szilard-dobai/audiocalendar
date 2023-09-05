8f28bb5f13482ce419ffb258578e780939187e20
Szilard DOBAI <dobai_szilard@yahoo.com>
2023-09-06 00:56:10 +0300

    Write spotify tokens on login.

--- app/src/hooks/useAuth.ts
+++ app/src/hooks/useAuth.ts
@@ -1,6 +1,7 @@
 import { Scopes } from "@spotify/web-api-ts-sdk";
 import { Session } from "@supabase/supabase-js";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useEffect } from "react";
 import useSupabase from "./useSupabase";
 
 export type AuthStatus = "loading" | "authenticated" | "unauthenticated";
@@ -22,6 +23,8 @@ export const useAuth = () => {
   const { mutate: login, isLoading: isLoggingIn } = useLogin();
   const { mutate: logout, isLoading: isLoggingOut } = useLogout();
   const { data: session, isLoading: isLoadingSession } = useSession();
+  useUpdateTokens();
+
   const isLoading = isLoggingIn || isLoggingOut || isLoadingSession;
 
   return {
@@ -37,20 +40,16 @@ const useLogin = () => {
   const supabase = useSupabase();
 
   return useMutation({
-    mutationFn: () =>
-      supabase.auth
-        .signInWithOAuth({
-          provider: "spotify",
-          options: {
-            scopes: Scopes.userRecents.join(" "),
-          },
-        })
-        .then(({ data, error }) => {
-          if (error) {
-            throw error;
-          }
-          return data;
-        }),
+    mutationFn: async () => {
+      const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: "spotify",
+        options: { scopes: Scopes.userRecents.join(" ") },
+      });
+      if (error) {
+        throw error;
+      }
+      return data;
+    },
     retry: false,
   });
 };
@@ -60,12 +59,12 @@ const useLogout = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: () =>
-      supabase.auth.signOut().then(({ error }) => {
-        if (error) {
-          throw error;
-        }
-      }),
+    mutationFn: async () => {
+      const { error } = await supabase.auth.signOut();
+      if (error) {
+        throw error;
+      }
+    },
     onSuccess: () => queryClient.invalidateQueries({ queryKey: ["session"] }),
     retry: false,
   });
@@ -76,13 +75,42 @@ const useSession = () => {
 
   return useQuery({
     queryKey: ["session"],
-    queryFn: () =>
-      supabase.auth.getSession().then(({ data, error }) => {
-        if (error) {
-          throw error;
-        }
-        return data.session;
-      }),
+    queryFn: async () => {
+      const { data, error } = await supabase.auth.getSession();
+      if (error) {
+        throw error;
+      }
+      return data.session;
+    },
     retry: false,
   });
+};
+
+const useUpdateTokens = () => {
+  const supabase = useSupabase();
+
+  useEffect(() => {
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((event, session) => {
+      if (session && event === "SIGNED_IN") {
+        void supabase
+          .from("spotify_tokens")
+          .upsert(
+            {
+              userId: session.user.id,
+              accessToken: session.provider_token,
+              refreshToken: session.provider_refresh_token,
+              expiresAt: session.expires_at,
+              expiresIn: session.expires_in,
+            },
+            { onConflict: "userId" }
+          )
+          // NOTE: without this then(), the call doesn't get made. If I make it async&await, execution stops and app is stuck in 'Loading'
+          .then(() => console.log("Set tokens!"));
+      }
+    });
+
+    return () => subscription.unsubscribe();
+  }, [supabase]);
 };

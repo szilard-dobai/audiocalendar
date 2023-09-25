52fd2eb99e4258aadf39d43e488a2653f7bac755
Szilard DOBAI <dobai_szilard@yahoo.com>
2023-09-26 00:39:05 +0300

    Remove console.log().

--- client/src/app/auth/callback/route.ts
+++ client/src/app/auth/callback/route.ts
@@ -7,7 +7,6 @@ export const GET = async (req: NextRequest) => {
   const { searchParams } = new URL(req.url);
 
   const code = searchParams.get("code");
-  console.log("code", code);
   if (code) {
     await supabase.auth.exchangeCodeForSession(code);
   }

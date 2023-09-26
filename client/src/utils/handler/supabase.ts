4f0dcecf79367891ba552682ff166863e8665684
Szilard DOBAI <dobai_szilard@yahoo.com>
2023-09-26 22:04:47 +0300

    Fix build crashing.

--- client/src/utils/handler/supabase.ts
+++ client/src/utils/handler/supabase.ts
@@ -2,5 +2,8 @@ import { Database } from "@audiocalendar/types";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 
-export const createSupabaseClient = () =>
-  createRouteHandlerClient<Database>({ cookies });
+export const createSupabaseClient = () => {
+  // NOTE: Without this weird hack, building the app crashes...
+  const cookiesData = cookies();
+  return createRouteHandlerClient<Database>({ cookies: () => cookiesData });
+};

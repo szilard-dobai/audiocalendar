997df2ed821fa417448075eb00550455becd39df
Szilard DOBAI <dobai_szilard@yahoo.com>
2023-09-26 22:50:00 +0300

    Fix wrong schema.

--- client/src/app/auth/spotify/schema.ts
+++ client/src/app/auth/spotify/schema.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 export const PostSpotifyInput = z.object({
   access_token: z.string(),
-  expires: z.string(),
-  expires_in: z.string(),
+  expires: z.number(),
+  expires_in: z.number(),
   refresh_token: z.string(),
 });
 export type PostSpotifyInput = z.infer<typeof PostSpotifyInput>;

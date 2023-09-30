115de4427a0dfe72eca1ffb39d7f15ab0f170e5f
Szilard DOBAI <dobai_szilard@yahoo.com>
2023-09-30 21:51:23 +0300

    Make logo responsive.

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,13 +1,15 @@
-import Image from "next/image";
 import Link from "next/link";
-import logo from "../../public/logo-with-text.svg";
 import Button from "./Button";
 
 const Navigation = () => {
   return (
     <nav className="py-3 px-4 sm:px-16 w-full rounded-b-lg border flex flex-row items-center justify-between text-white bg-brand mb-10">
       <Link href="/" className="text-white">
-        <Image src={logo} alt="logo" height={45} />
+        <picture>
+          {/* // TODO: Figure out how to make this work with next/image */}
+          <source media="(min-width: 640px)" srcSet="./logo-with-text.svg" />
+          <img src="./logo.svg" alt="logo" className="h-11" />
+        </picture>
       </Link>
 
       <Link href="/account" passHref legacyBehavior>

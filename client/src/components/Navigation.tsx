406cae4483d7d8aa8a775725c0d3d0e99f0a2425
Szilard DOBAI <dobai_szilard@yahoo.com>
2023-09-30 20:41:18 +0300

    Add logo

--- client/src/components/Navigation.tsx
+++ client/src/components/Navigation.tsx
@@ -1,11 +1,13 @@
+import Image from "next/image";
 import Link from "next/link";
+import logo from "../../public/logo-with-text.svg";
 import Button from "./Button";
 
 const Navigation = () => {
   return (
     <nav className="py-3 px-4 sm:px-16 w-full rounded-b-lg border flex flex-row items-center justify-between text-white bg-brand mb-10">
       <Link href="/" className="text-white">
-        [LOGO]
+        <Image src={logo} alt="logo" height={45} />
       </Link>
 
       <Link href="/account" passHref legacyBehavior>

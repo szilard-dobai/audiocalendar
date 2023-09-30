900dabbbd87415eefaa6160b884ac6b37e529c07
Szilard DOBAI <dobai_szilard@yahoo.com>
2023-09-30 21:01:38 +0300

    Add metadata.

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -10,7 +10,27 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Audiocalendar",
-  description: "Never lose a song again!",
+  description: "Never Miss a Beat",
+  manifest: "./site.webmanifest",
+  icons: [
+    {
+      url: "./favicon-32x32.png",
+      sizes: "32x32",
+      rel: "icon",
+      type: "image/png",
+    },
+    {
+      url: "./favicon-16x16.png",
+      sizes: "16x16",
+      rel: "icon",
+      type: "image/png",
+    },
+    {
+      url: "./apple-touch-icon.png",
+      sizes: "180x180",
+      rel: "apple-touch-icon",
+    },
+  ],
 };
 
 const RootLayout = ({ children }: PropsWithChildren) => {

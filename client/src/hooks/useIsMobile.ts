db100aafec02ba10a97a66b015191e059e50e033
Szilard DOBAI <dobai_szilard@yahoo.com>
2023-10-14 01:36:05 +0300

    Add new hook.

---
+++ client/src/hooks/useIsMobile.ts
@@ -0,0 +1,19 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export const useIsMobile = () => {
+  const [width, setWidth] = useState(window.innerWidth);
+  const handleWindowSizeChange = () => {
+    setWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowSizeChange);
+    return () => {
+      window.removeEventListener("resize", handleWindowSizeChange);
+    };
+  }, []);
+
+  return width <= 768;
+};

e9b6fa638b3a39120e265d8c02ce7c31b9e1b336
Szilard DOBAI <dobai_szilard@yahoo.com>
2023-10-16 20:45:44 +0300

    Attempt to fix logout bug.

--- client/src/app/account/page.tsx
+++ client/src/app/account/page.tsx
@@ -17,6 +17,9 @@ const TODAY = dayjs();
 
 type SongWithCount = Song & { count: number };
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 const AccountPage = () => {
   const isMobile = useIsMobile();
   const [referenceDate, setReferenceDate] = useState(dayjs());

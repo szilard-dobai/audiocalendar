662e04229d8369a8ab3f944d1afbb4d04fc839ad
Szilard DOBAI <dobai_szilard@yahoo.com>
2023-10-26 18:04:43 +0300

    Revert change. Fix actual issue.

--- supabase/functions/_shared/zod.ts
+++ supabase/functions/_shared/zod.ts
@@ -1,1 +1,10 @@
-export { type TypeToZod } from "../../../packages/types/zod.ts";
+import { z } from "https://deno.land/x/zod@v3.22.4/mod.ts";
+
+// Source: https://github.com/colinhacks/zod/issues/53#issuecomment-1681090113
+export type TypeToZod<T> = {
+  [K in keyof T]: T[K] extends string | number | boolean | null | undefined
+    ? undefined extends T[K]
+      ? z.ZodOptional<z.ZodType<Exclude<T[K], undefined>>>
+      : z.ZodType<T[K]>
+    : z.ZodObject<TypeToZod<T[K]>>;
+};
